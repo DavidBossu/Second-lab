34864de60ac09b7e8fa3f6dffdcfccd23d51317b
David <david.bayrakci@isa.utm.md>
2021-10-17 17:56:44 +0300

    Task2

--- src/utm/Task3/Main.java
+++ src/utm/Task3/Main.java
@@ -5,8 +5,10 @@ public class Main {
     public static void main(String[] args) {
         SmartPhone samsung = new SmartPhone("Samsung", "Alb", 1200, 0.580);
         samsung.tell();
+
         SmartPhone LG = new SmartPhone("lg", "rosu", 100, 2);
         LG.tell();
+
         Additional samsungS8 = new Additional("Samsung_S8", "Alb", 1200, 0.580,"Are","Are");
         samsungS8.customPhone();
     }
